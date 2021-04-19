719184e8d9c77805bb68aff14103bebab21f3031
LilMonk <rahulsahoo66@gmail.com>
2021-04-19 21:01:13 +0530

    First Commit

---
+++ src/test/java/com/grp6/reddit_clone/RedditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grp6.reddit_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
