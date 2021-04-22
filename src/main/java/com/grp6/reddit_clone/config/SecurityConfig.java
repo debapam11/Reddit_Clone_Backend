d2564ed5cd8a22e1c91162373485f114e19b9410
Debapam Pal <debapam11@gmail.com>
2021-04-22 13:08:59 +0530

    minor fix

--- src/main/java/com/grp6/reddit_clone/config/SecurityConfig.java
+++ src/main/java/com/grp6/reddit_clone/config/SecurityConfig.java
@@ -2,10 +2,11 @@ package com.grp6.reddit_clone.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity httpSecurity) throws Exception {
