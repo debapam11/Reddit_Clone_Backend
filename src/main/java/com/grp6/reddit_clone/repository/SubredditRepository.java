cd3334f1c0f633e100a67f68fa7abd3e93febdb6
Debapam Pal <debapam11@gmail.com>
2021-04-23 10:33:07 +0530

    testing secure entry

---
+++ src/main/java/com/grp6/reddit_clone/repository/SubredditRepository.java
@@ -0,0 +1,13 @@
+package com.grp6.reddit_clone.repository;
+
+import com.grp6.reddit_clone.model.Subreddit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SubredditRepository extends JpaRepository<Subreddit, Long> {
+
+    Optional<Subreddit> findByName(String subredditName);
+}
